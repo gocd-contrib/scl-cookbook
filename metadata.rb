aeef413a5601fb573460e8ed603b156191afb014
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2016-08-24 13:55:11 +0530

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'software-collections'
+maintainer       'ThoughtWorks, Inc.'
+maintainer_email 'go-cd-dev@googlegroups.com'
+license          'MIT'
+description      'Installs/Configures scl'
+version          '0.1.0'
+
+depends 'yum'
